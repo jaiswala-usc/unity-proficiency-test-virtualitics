b847edff311023abc0ea2d2c73ba8bf5784f4755
jaiswala-usc <abhishek.cse16@gmail.com>
2019-04-07 23:35:37 -0700

    Parse CSV Done

---
+++ unity-proficiency-test-virtualitics/Assets/DataPointSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataPointSpawner : MonoBehaviour {
+
+	public string testcsv;
+
+	private List<Dictionary<string, object>> datapoints;
+
+	// Use this for initialization
+	void Start () {
+
+		datapoints = ParseCSVData.Read(testcsv);
+
+		Debug.Log(datapoints);
+
+		List<string> columns = new List<string> (datapoints[1].Keys);
+		Debug.Log(datapoints.Count);
+
+		foreach (string column in columns)
+		{
+			Debug.Log("Col" + column);
+		}
+		
+	}
+	
+	// Update is called once per frame
+	// void Update () {
+		
+	// }
+}
