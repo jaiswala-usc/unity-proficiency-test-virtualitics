6c63a39d3e1e78890afe9b68a7bc3c0e785619cd
jaiswala-usc <abhishek.cse16@gmail.com>
2019-04-12 22:23:56 -0700

    Revert "Cleaned up project structure"
    This reverts commit e297d6a50205c7e57b98040c68cc62baeb8dcf65.

--- unity-proficiency-test-virtualitics/Assets/DataPointSpawner.cs
+++ unity-proficiency-test-virtualitics/Assets/DataPointSpawner.cs
@@ -1,17 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
 // using static System.Convert;
 
 public class DataPointSpawner : MonoBehaviour {
-
-
-	static string SPLIT_RE = @",(?=(?:[^""]*""[^""]*"")*(?![^""]*""))";
-    static string LINE_SPLIT_RE = @"\r\n|\n\r|\n|\r";
-    static char[] TRIM_CHARS = { '\"' };
 
 	public string testcsv;
 
@@ -50,7 +44,7 @@ public class DataPointSpawner : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-		datapoints = Read(testcsv);
+		datapoints = ParseCSVData.Read(testcsv);
 
 
 		List<string> columns = new List<string> (datapoints[1].Keys);
@@ -152,40 +146,5 @@ public class DataPointSpawner : MonoBehaviour {
 		yCoord.text = "Y-Coordinate:  " + dataSphere.transform.position.y.ToString();
 		zCoord.text = "Z-Coordinate:  " + dataSphere.transform.position.z.ToString();
 		colorText.text = "Color:  " + ((dataSphere.GetComponent<Renderer>().material.color == Color.green)?"Green":((dataSphere.GetComponent<Renderer>().material.color == Color.blue)?"Blue":"Red"));
-	}
-
-	public static List<Dictionary<string, object>> Read(string file) {
-
-		var list = new List<Dictionary<string, object>>();
-        TextAsset data = Resources.Load (file) as TextAsset;
- 
-        var lines = Regex.Split (data.text, LINE_SPLIT_RE);
- 
-        if(lines.Length <= 1) return list;
- 
-        var header = Regex.Split(lines[0], SPLIT_RE);
-        for(var i=1; i < lines.Length; i++) {
- 
-            var values = Regex.Split(lines[i], SPLIT_RE);
-            if(values.Length == 0 ||values[0] == "") continue;
- 
-            var entry = new Dictionary<string, object>();
-            for(var j=0; j < header.Length && j < values.Length; j++ ) {
-                string value = values[j];
-                value = value.TrimStart(TRIM_CHARS).TrimEnd(TRIM_CHARS).Replace("\\", "");
-                object finalvalue = value;
-                int n;
-                float f;
-                if(int.TryParse(value, out n)) {
-                    finalvalue = n;
-                } else if (float.TryParse(value, out f)) {
-                    finalvalue = f;
-                }
-                entry[header[j]] = finalvalue;
-            }
-            list.Add (entry);
-        }
-        return list;
-
 	}
 }
